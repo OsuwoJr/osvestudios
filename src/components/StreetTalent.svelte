af5705b2bd41ef550993afb18758e67a48e9ff4f
OsuwoJr <osuwoodongo@gmail.com>
2025-04-24 19:03:40 +0300

    remove title emoji

--- src/components/StreetTalent.svelte
+++ src/components/StreetTalent.svelte
@@ -33,7 +33,7 @@
 </script>
 
 <section id="street-talent" class="street-talent">
-    <h2 class="section-title">🔥 The Troublers Street Talent</h2>
+    <h2 class="section-title">The Troublers Street Talent</h2>
     <p class="mt-4 text-lg">Discover and support the raw talents of Nairobi’s streets.</p>
     <Button text="Join Us" link="/street-talent" />
 
